fbca9b283212d33db06d377d583c19c2ab3685e9
EC2 Default User <ec2-user@ip-172-31-82-61.ec2.internal>
2019-12-05 18:01:40 +0000

    ordersshowing

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,8 @@ class OrdersController < ApplicationController
   # GET /orders/1
   # GET /orders/1.json
   def show
+    @orderitems = Orderitem.all
+    @orderitems = Orderitem.where(order_id: params[:id])
   end
 
   # GET /orders/new
